0f19950b430f7c387ea8999b2fc77b2771e94c90
Criss Martin <nitramssirc@yahoo.com>
2020-05-20 07:32:31 -0500

    Added example components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {Button, Alert} from 'react-bootstrap'
 import './App.css';
 import './bootstrap.min.css' //See https://bootswatch.com/superhero/ for theme documentation
 
 function App() {
+  const [show, setShow] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {show &&
+        <Alert variant="warning" onClose={()=>setShow(false)} dismissible>
+          Launching Global Thermonuclear War
+        </Alert>
+      }
+      <Button variant="danger" onClick={() => setShow(true)}>Shall We Play A Game?</Button>      
+    </>
   );
 }
 
